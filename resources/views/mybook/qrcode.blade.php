7e670ff9ccc6032abbc855c0b2a9056db1e0f7b9
Patrickmarionatha <74396182+Patrickmarionatha@users.noreply.github.com>
2022-05-31 17:32:19 +0700

    updated

--- resources/views/mybook/qrcode.blade.php
+++ resources/views/mybook/qrcode.blade.php
@@ -4,10 +4,14 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">My QR Code</h1>
     </div>
-
-    <div class="card col-lg-auto">
-        <div class="card-body">
-            {{ QrCode::size(300)->generate(DB::table('books')->select('id')->where('id', $id)->get()) }}
+    <div class="row">
+        <div class="card col-3">
+            <div class="card-head text-center">
+                QR Code Book {{ $id }}
+            </div>
+            <div class="card-body justify-content-center">
+                {{ QrCode::size(270)->generate(DB::table('books')->select('id')->where('id', $id)->get()) }}
+            </div>
         </div>
     </div>
 @endsection
