f7eef0856d09b5ec696883e528a8e5ab8a1db68a
Patrickmarionatha <74396182+Patrickmarionatha@users.noreply.github.com>
2022-06-01 12:54:37 +0700

    updated

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -6,21 +6,21 @@
         <div class="container position-relative top-50 translate-middle-y">
             <!-- Service detail -->
             <div class="row justify-content-center">
-                <div class="col-4">
+                <div class="col-md-4">
                     <div class="card bg-light text-dark text-center" style="width: 18rem;">
                         <div class="card-body">
                             <a class="btn btn-light btn-lg" href="/cleaning">Cleaning</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-md-4">
                     <div class="card bg-light text-dark text-center" style="width: 18rem;">
                         <div class="card-body">
                             <a class="btn btn-light btn-lg" href="/detailing">Detailing</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-md-4">
                     <div class="card bg-light text-dark text-center" style="width: 18rem;">
                         <div class="card-body">
                             <a class="btn btn-light btn-lg" href="/coating">Coating</a>
